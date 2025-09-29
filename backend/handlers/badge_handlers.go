566d079a7db7e128dac0f9add43d845bc213739a
isak29 <lampell.i@gmail.com>
2025-09-29 19:53:09 +0200

    User_Badges
    userBages tillagd, strular dock då det inte går att lägga till några user badges i db?? osäkrt varför

--- backend/handlers/badge_handlers.go
+++ backend/handlers/badge_handlers.go
@@ -10,6 +10,10 @@ type BadgeHandler struct {
 	Repo *database.BadgeRepository
 }
 
+type UserBadgeHandler struct {
+	Repo *database.UserBadgeRepository
+}
+
 // GetAllUsersHandler hanterar förfrågningar till /api/v1/users
 func (h *BadgeHandler) GetAllBadgesHandler(w http.ResponseWriter, r *http.Request) {
 	// Anropa funktionen ni precis skapade i er repository.
@@ -25,4 +29,20 @@ func (h *BadgeHandler) GetAllBadgesHandler(w http.ResponseWriter, r *http.Reques
 
 	// Omvandla er lista av användare till JSON och skicka den.
 	json.NewEncoder(w).Encode(badges)
+}
+
+// GetAllUserBadgesHandler hanterar förfrågningar till /api/v1/userbadges
+func (h *UserBadgeHandler) GetAllUserBadgesHandler(w http.ResponseWriter, r *http.Request) {
+	// Anropa funktionen i repository
+	userBadges, err := h.Repo.GetAllUserBadges()
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+
+	// Sätt header för JSON
+	w.Header().Set("Content-Type", "application/json")
+
+	// Skicka tillbaka resultatet som JSON
+	json.NewEncoder(w).Encode(userBadges)
 }
