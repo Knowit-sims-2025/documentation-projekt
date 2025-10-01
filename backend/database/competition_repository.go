9c0ce1525bb2a36fd9e826d858b2176fb580f0e3
Taha Khudher <taha.khudher@gmail.com>
2025-10-01 11:30:23 +0200

    cempetition repo, behöver några fler funktioner, skapa handlers och router

--- backend/database/competition_repository.go
+++ backend/database/competition_repository.go
@@ -52,3 +52,40 @@ func (r *CompetitionRepository) GetAllCompetitions() ([]models.Competition, erro
 
 	return competitions, nil
 }
+
+func (r *CompetitionRepository) CreateCompetition(c *models.Competition) (int64, error) {
+	query :=  `INSERT INTO competitions (name, description, start_date, end_date, created_by_user_id) VALUES ($1, $2, $3, $4, $5) RETURNING id`
+	err := r.DB.QueryRow(query, c.Name, c.Description, c.StartDate, c.EndDate, c.CreatedByUserID).Scan(&c.ID)
+	if err != nil {
+		return 0, err
+	}
+	return c.ID, nil
+}
+
+func (r * CompetitionRepository) GetCompetitionByID(id int64) (*models.Competition, error) {
+	row := r.DB.QueryRow(`SELECT id, name, description, start_date, end_date, created_by_user_id, created_at FROM competitions WHERE id = $1`, id)
+	var comp models.Competition
+	err := row.Scan(
+		&comp.ID,
+		&comp.Name,
+		&comp.Description,
+		&comp.StartDate,
+		&comp.EndDate,
+		&comp.CreatedAt,
+		&comp.CreatedByUserID,
+	)
+	if err != nil {
+		if err == sql.ErrNoRows {
+			return nil, nil // Ingen tävling hittades med det ID:t
+		}
+		return nil, err
+
+	}
+	return &comp, nil
+}
+
+func (r *CompetitionRepository) UpdateCompetition(c *models.Competition) error {
+	_, err := r.DB.Exec(`UPDATE competitions SET name = $1, description = $2, start_date = $3, end_date = $4 WHERE id = $5`,
+		c.Name, c.Description, c.StartDate, c.EndDate, c.ID)
+	return err
+}
