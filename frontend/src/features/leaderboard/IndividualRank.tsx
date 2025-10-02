df50dc90b1c179cb660edb857f0ae73ecad7d32e
Vergiliuz <christian.strid@gmail.com>
2025-10-02 11:39:49 +0200

    Fixat "titel" på leaderboards med förklaring vid hoover

--- frontend/src/features/leaderboard/IndividualRank.tsx
+++ frontend/src/features/leaderboard/IndividualRank.tsx
@@ -4,11 +4,28 @@ import { Avatar } from "../../components/Avatar";
 export function IndividualRank({ user }: { user: User }) {
   return (
     <li className="leaderboard-item">
-      <span className="leaderboard-rank">{user.rank}.</span>
+      <span
+        className="leaderboard-rank"
+        title={`${user.displayName} is rank ${user.rank}`}
+      >
+        {user.rank}.
+      </span>
       <Avatar name={user.displayName} src={user.avatarUrl} />
-      <span className="leaderboard-name">{user.displayName}</span>
-      <span className="leaderboard-admin">{user.isAdmin ? "⭐" : ""}</span>
-      <span className="leaderboard-points">{user.totalPoints} p</span>
+      <span
+        className="leaderboard-name"
+        title="Click on username to view profile"
+      >
+        {user.displayName}
+      </span>
+      <span className="leaderboard-admin" title="Admin">
+        {user.isAdmin ? "⭐" : ""}
+      </span>
+      <span
+        className="leaderboard-points"
+        title={`${user.displayName}: ${user.totalPoints} poäng`}
+      >
+        {user.totalPoints} p
+      </span>
     </li>
   );
 }
