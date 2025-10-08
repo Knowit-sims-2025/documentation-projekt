6332ddd18e2929203db2e4563db217b010c0260b
Vergiliuz <christian.strid@gmail.com>
2025-10-08 09:00:59 +0200

    tog bort router.go

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,10 @@
 import { defineConfig } from 'vite'
-import svgr from "vite-plugin-svgr";
+// import svgr from "vite-plugin-svgr";
 import react from '@vitejs/plugin-react'
 
 
 export default defineConfig({
-  plugins: [svgr(), react()],
+  plugins: [react()],
   server: {
     proxy: {
       // Proxy alla förfrågningar som börjar med '/api' till din Go-backend
