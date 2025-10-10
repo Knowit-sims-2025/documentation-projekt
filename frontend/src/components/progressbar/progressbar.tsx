cb71564de76fa4a63f542b6f2822ee2b8ce899b5
MelH01 <82213432+MelH01@users.noreply.github.com>
2025-10-10 11:51:54 +0200

    Adjusted visability of labels in progress-bar

--- frontend/src/components/progressbar/progressbar.tsx
+++ frontend/src/components/progressbar/progressbar.tsx
@@ -1,25 +1,22 @@
 import React from "react";
 
 interface ProgressBarProps {
-  value: number; // current user progress
+  value: number;      // current user progress
   max?: number; // next threshold
-  min?: number; // previous threshold
-  label?: string; // label of widget
-  src?: string; // optional image URL
+  min?: number;      // previous threshold
+  label?: string;     // label of widget
+  src?: string;       // optional image URL
 }
 const ProgressBar: React.FC<ProgressBarProps> = ({
   value,
   max = 1,
-  min = 1,
-  label,
-  src = undefined,
+  min = 0,
+  label, 
+  src = undefined
 }) => {
-  const percentage = Math.min(
-    Math.max(((value - min) / (max - min)) * 100, 0),
-    100
-  );
+  const percentage = Math.min(Math.max(((value-min) / (max - min)) * 100, 0), 100);
   const hideLabel = percentage < 10;
-  return (
+   return (
     <div className="progress-card">
       {label && <span className="progress-label">{label}</span>}
       {src && <img src={src} alt="" />}
@@ -28,17 +25,17 @@ const ProgressBar: React.FC<ProgressBarProps> = ({
           className="progress-bar"
           style={{
             width: `${percentage}%`,
-          }}
-        >
-          {!hideLabel && <span>{value}</span>}
-        </div>
+          }}>
+            {!hideLabel && <span>{value}</span>}
+            </div>
       </div>
       <div className="progress-numbers">
-        {label && <span>{min}</span>}
+        {label && (min !== 0 ? <span>{min}</span> : <span className="invisible" style={{ visibility: "hidden" }}>0</span>)}
         {label && <span>{max}</span>}
-      </div>
+    </div>
     </div>
   );
 };
+
 
 export default ProgressBar;
